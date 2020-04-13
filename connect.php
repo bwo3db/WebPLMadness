d9eb17af13595e889e192e366076f3803a3483b8
bwo3db <bwo3db@virginia.edu>
2020-04-13 17:24:33 -0400

    added the proper connect.php file

---
+++ connect.php
@@ -0,0 +1,20 @@
+<?php
+    $dbname = 'webplmadness';
+    $hostname = '127.0.0.1';
+
+    $username = 'root';
+    $password = '';
+
+    $dsn = "mysql:host=$hostname;dbname=$dbname";
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+        //echo  "<p>It worked!<p>";
+    } catch (PDOException $e) {
+        $error_message = $e->getMessage();
+        echo "<p>An error occurred while connecting to the database: $error_message </p>";
+    } catch (Exception $e) {
+        $error_message = $e->getMessage();
+        echo "<p>Error message: $error_message </p>";
+    }
+?>
