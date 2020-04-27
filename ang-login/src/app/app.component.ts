6b851830cdecf565281c34b0a3f3781d9c37031f
Nathan Park <nyp5aa@virginia.edu>
2020-04-27 11:30:26 -0400

    trying to send data to backend.php

--- ang-login/src/app/app.component.ts
+++ ang-login/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -9,19 +10,45 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent {
   title = 'ang-login';
+
+  userModel = new User('', '');
+
+  data_sumbitted = '';
+
+  response_data = new User('', '');
+
   constructor(private http: HttpClient) {
   }
-  sendPost(email: any, password: any): Observable<any> {
-    console.log(email);
-    console.log(password);
-    return this.http.post("http://localhost/webplmadness/backend_login.php", email, { responseType: 'text' });
-  }
-  subPost(data: any, data2: any) {
-    console.log(data)
-    this.sendPost(data, data2).subscribe(
-      res => {
-        console.log(res);
-      }
-    );
+
+  // sendPost(email: any, password: any): Observable<any> {
+  //   console.log(email);
+  //   console.log(password);
+  //   return this.http.post("http://localhost/webplmadness/backend_login.php", email, { responseType: 'text' });
+  // }
+
+  subPost(form: any) {
+    console.log(form)
+    this.data_sumbitted = form;
+
+    // Convert the form data to json format
+    let params = JSON.stringify(form);
+
+    console.log(params);
+
+    this.http.post('http://localhost/webplmadness/backend_login.php', params)
+      .subscribe(
+        res => {
+          console.log(res);
+        }
+      );
+    // .subscribe((data) => {
+    //   console.log(data);
+    //   // Receive a response successfully, do something here
+    //   // console.log('Response from backend ', data);
+    //   // this.response_data = data;     // assign response to responsedata property to bind to screen later
+    // }, (error) => {
+    //   // An error occurs, handle an error in some way
+    //   console.log('Error ', error);
+    // })
   }
 }
--- backend_login.php
+++ backend_login.php
@@ -5,44 +5,62 @@ header('Access-Control-Allow-Headers: X-Requested-With, Content-Type, Origin, Au
 require('connect.php');
 session_start();
 
-
-$postdata = file_get_contents("php://input");
+// $postdata = file_get_contents("php://input");
 // echo "this is the post data recieved by php: " . "'" . $postdata . "'";
 
-function login($email, $password) {
-    global $db;
+// retrieve data from the request
+$postdata = file_get_contents("php://input");
 
-    $query = "SELECT * FROM maintable WHERE email = :email";
-    $statement = $db -> prepare($query);
-    $statement -> bindValue(':email', $email);
-    $statement -> execute();
+// Process data
+// (this example simply extracts the data and restructures them back)
 
-    $result = $statement -> fetch();
-    $statement -> closeCursor();
+// Extract json format to PHP array
+$request = json_decode($postdata);
 
-
-    return $result;
+$data = [];
+$data[0]['length'] = $content_length;
+foreach ($request as $k => $v)
+{
+  $data[0]['post_'.$k] = $v;
 }
 
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    echo $postdata;
-    // $result = login($_POST['email'], $_POST['pwd']);
-    // $input_pwd = htmlspecialchars($_POST['pwd']);
-    // if(empty($result)) {
-    //     echo '<br/><p style="color: red; text-align: center; font-size: large;">Incorrect email or password</p>';
-    // }
-    // else if(password_verify($input_pwd, $result[2])){
-    //     echo "password matches <br>";
-    //     // Change this to where ever you want to person to go to
-    //     $_SESSION["username"] = $result["username"];
-    //     $_SESSION["email"] = $result["email"];
-    //     $_SESSION["bracket"] = $result["bracket"];
-    //     $_SESSION["winner"] = $result["winner"];
-    //     header("Location: index.php");
-    //   }
-    // else{
-    //     echo '<br/><p style="color: red; text-align: center; font-size: large;">Password does not match</p>';
-    // }
-}
+// Send response (in json format) back the front end
+echo json_encode(['content'=>$data]);
+
+// function login($email, $password) {
+//     global $db;
+
+//     $query = "SELECT * FROM maintable WHERE email = :email";
+//     $statement = $db -> prepare($query);
+//     $statement -> bindValue(':email', $email);
+//     $statement -> execute();
+
+//     $result = $statement -> fetch();
+//     $statement -> closeCursor();
+
+
+//     return $result;
+// }
+
+// if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+//     echo $postdata;
+//     $result = login($_POST['email'], $_POST['pwd']);
+//     $input_pwd = htmlspecialchars($_POST['pwd']);
+//     if(empty($result)) {
+//         echo '<br/><p style="color: red; text-align: center; font-size: large;">Incorrect email or password</p>';
+//     }
+//     else if(password_verify($input_pwd, $result[2])){
+//         echo "password matches <br>";
+//         // Change this to where ever you want to person to go to
+//         $_SESSION["username"] = $result["username"];
+//         $_SESSION["email"] = $result["email"];
+//         $_SESSION["bracket"] = $result["bracket"];
+//         $_SESSION["winner"] = $result["winner"];
+//         header("Location: index.php");
+//       }
+//     else{
+//         echo '<br/><p style="color: red; text-align: center; font-size: large;">Password does not match</p>';
+//     }
+// }
 
 ?>
